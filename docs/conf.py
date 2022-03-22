bbf074e4e541d06c68f980bfc54347bcacaaafce
Melvin Wong <m.j.w.wong@tue.nl>
2022-03-22 15:39:16 +0100

    update

--- docs/conf.py
+++ docs/conf.py
@@ -75,3 +75,9 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Options for LaTeX output ------------------------------------------
+
+latex_documents = [
+ ('index', 'index.tex', u'icmcreslogit', u'Melvin Wong', 'howto'),
+]
